8e40651df7975586a147a36f8122ee27a6710ed4
Snehal <snehaldeshmukh150@gmail.com>
2025-07-30 12:11:23 +0530

    Today Work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminDashboard from "./components/Admin/AdminDashboard";
 import MemberDashboard from "./components/Member/MemberDashboard";
+import ManageDrugs from "./components/Admin/ManageDrugs";
+import ManageMembers from "./components/Admin/ManageMembers";
+import AddMember from "./components/Admin/AddMember";
+import AddDrug from "./components/Admin/AddDrug";
 
 function App() {
   return (
@@ -11,7 +15,12 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/drugs" element={<ManageDrugs />} />
+        <Route path="/admin/members" element={<ManageMembers />} />
+        <Route path="/admin/add-member" element={<AddMember />} />
         <Route path="/member" element={<MemberDashboard />} />
+        <Route path="/admin/add-drug" element={<AddDrug />} />
+
       </Routes>
     </BrowserRouter>
 
